a885baa76c45f8946564d6937b478111865b0b08
Ron Dagdag <3879302+rondagdag@users.noreply.github.com>
2024-11-12 16:36:48 +0000

    ollama

--- demo3/todos-app/app/api/copilotkit/route.ts
+++ demo3/todos-app/app/api/copilotkit/route.ts
@@ -5,16 +5,21 @@ import {
   } from '@copilotkit/runtime';
   import OpenAI from 'openai/index.mjs';
   import { NextRequest } from 'next/server';
-   
+  import { getGroqAdapter, getLangChainAzureOpenAIAdapter, getLangChainOllamaAdapter } from "./adapter"
+
   const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
-  const serviceAdapter = new OpenAIAdapter({ openai });
   const runtime = new CopilotRuntime();
    
   export const POST = async (req: NextRequest) => {
     const { handleRequest } = copilotRuntimeNextJSAppRouterEndpoint({
       runtime,
-      serviceAdapter,
-      endpoint: '/api/copilotkit',
+      //serviceAdapter: await getLangChainOpenAIAdapter(),
+      //serviceAdapter: await getOpenAIAdapter(),
+      //serviceAdapter: await getLangChainOllamaAdapter(),
+      //serviceAdapter: await getLangChainAzureOpenAIAdapter(),
+      //serviceAdapter: await getAzureOpenAIAdapter(),
+      serviceAdapter: await getLangChainOllamaAdapter(),
+      endpoint: req.nextUrl.pathname,
     });
    
     return handleRequest(req);
