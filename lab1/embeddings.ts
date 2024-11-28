fbaa0f17131cabf827beb7fb6e47c627301994d8
rondagdag <rlyle78@gmail.com>
2024-11-28 12:11:16 +0800

    embeddings

--- lab1/embeddings.ts
+++ lab1/embeddings.ts
@@ -10,7 +10,16 @@ import { AzureOpenAIEmbeddings } from "@langchain/openai";
 
 // export const embeddings = new AzureOpenAIEmbeddings();
 
+// import { OpenAIEmbeddings } from "@langchain/openai";
+// export const embeddings = new OpenAIEmbeddings({
+//     model: "text-embedding-3-small",
+//   });
+
 import { OpenAIEmbeddings } from "@langchain/openai";
 export const embeddings = new OpenAIEmbeddings({
     model: "text-embedding-3-small",
+    apiKey: process.env.GITHUB_OPENAI_API_KEY,
+    configuration: {
+      baseURL: 'https://models.inference.ai.azure.com'
+    }
   });
